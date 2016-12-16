c652ef0ff177227f27596f3bee266f6a7a908c9d
Raed Atoui <ratoui@hugeinc.com>
2016-12-16 11:00:30 -0500

    camera struct

--- utils/camera.go
+++ utils/camera.go
@@ -5,6 +5,12 @@ import (
 	"math"
 )
 
+const (
+	FORWARD = iota
+	BACKWARD
+	LEFT
+	RIGHT
+)
 const YAW = -90.0
 const PITCH = 0.0
 const SPEED = 3.0
@@ -70,6 +76,57 @@ func (cam *Camera)  GetViewMatrix() mgl32.Mat4 {
 		eye.X(), eye.Y(), eye.Z(),
 		center.X(), center.Y(), center.Z(),
 		up.X(), up.Y(), up.Z())
+}
+
+// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+func (cam *Camera) ProcessKeyboard(direction int, deltaTime float32) {
+	velocity := float32(cam.MovementSpeed) * deltaTime
+	if (direction == FORWARD) {
+		cam.Position = cam.Position.Add(cam.Front.Mul(velocity))
+	}
+	if (direction == BACKWARD) {
+		cam.Position = cam.Position.Sub(cam.Front.Mul(velocity))
+	}
+	if (direction == LEFT) {
+		cam.Position = cam.Position.Sub(cam.Right.Mul(velocity))
+	}
+	if (direction == RIGHT) {
+		cam.Position = cam.Position.Add(cam.Right.Mul(velocity))
+	}
+}
+
+// Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+func (cam *Camera) ProcessMouseMovement(xoffset, yoffset float32, constrainPitch bool) {
+	xoffset *= cam.MouseSensitivity
+	yoffset *= cam.MouseSensitivity
+
+	cam.Yaw   += xoffset
+	cam.Pitch += yoffset
+
+	// Make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (constrainPitch){
+		if (cam.Pitch > 89.0) {
+			cam.Pitch = 89.0
+		}
+		if (cam.Pitch < -89.0) {
+			cam.Pitch = -89.0
+		}
+	}
+	// Update Front, Right and Up Vectors using the updated Eular angles
+	cam.updateCameraVectors()
+}
+
+// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+func (cam *Camera) ProcessMouseScroll(yoffset float32) {
+	if (cam.Zoom >= 1.0 && cam.Zoom <= 45.0) {
+		cam.Zoom -= yoffset
+	}
+	if (cam.Zoom <= 1.0) {
+		cam.Zoom = 1.0
+	}
+	if (cam.Zoom >= 45.0) {
+		cam.Zoom = 45.0
+	}
 }
 
 func (cam *Camera) updateCameraVectors() {
